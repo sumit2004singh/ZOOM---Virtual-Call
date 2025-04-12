32b730fe74d0039bd46cd93bfbbb0c428cd0e87e
sumit2004singh <sumitsingh981156@gmail.com>
2025-04-12 17:29:24 +0530

    Refactor App component structure and fix minor syntax errors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,35 @@
 import './App.css';
-import {Route, BrowserRouter, Routes } from 'react-router-dom';
-import LandingPage from "./pages/landing"
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import LandingPage from './pages/landing';
 import Authentication from './pages/authentication';
 import { AuthProvider } from './contexts/AuthContext';
 import VideoMeetComponent from './pages/VideoMeet';
+import HomeComponent from './pages/home';
 import History from './pages/history';
-import HomeComponenet from './pages/home'
 
 function App() {
   return (
-    <>
-    
-    <BrowserRouter>
-      <AuthProvider>
-        <Routes>
-          <Route path='/' element={<LandingPage/>}/>
-          <Route path='/auth' element={<Authentication/>}/>
-          <Route path='/home's element={<HomeComponenet />} />
-          <Route path='/history' elememt={<History/>} />
-          <Route path='/:url' element={<VideoMeetComponent/>}/>
-          
-        </Routes>
-      </AuthProvider>
-    </BrowserRouter>
-    
-    </>
+    <div className="App">
+
+      <Router>
+
+        <AuthProvider>
+
+
+          <Routes>
+
+            <Route path='/' element={<LandingPage />} />
+
+            <Route path='/auth' element={<Authentication />} />
+
+            <Route path='/home's element={<HomeComponent />} />
+            <Route path='/history' element={<History />} />
+            <Route path='/:url' element={<VideoMeetComponent />} />
+          </Routes>
+        </AuthProvider>
+
+      </Router>
+    </div>
   );
 }
 
